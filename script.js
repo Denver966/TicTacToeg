eb18f61e3567111393b40afe53b353f7a8ee5da6
Abdullah Qureshi <denverreachers@gmail.com>
2025-08-08 15:57:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ let turn = 'X'
 let gameover = false
 //function to change the turn
 
-const changeTurn = ()=>{
-    return turn === 'X' ? 'O': 'X'
+const changeTurn = () => {
+    return turn === 'X' ? 'O' : 'X'
 }
 
 
 //function to check for a win
 
-const checkWin = ()=>{
+const checkWin = () => {
+
     let boxtexts = document.getElementsByClassName('boxtext')
     let wins = [
         [0, 1, 2, 0, 4.4, 0],
@@ -24,12 +25,18 @@ const checkWin = ()=>{
         [2, 4, 6, -0.1, 15, 135],
     ]
 
-    wins.forEach((e)=>{
-        if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')){
+    wins.forEach((e) => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')) {
             document.querySelector('.info').innerText = boxtexts[e[0]].innerText + 'Won'
             gameover = true
             document.querySelector(".line").style.width = "30vw";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+
+
+            const messageDiv = document.getElementById("winnerMessage");
+            messageDiv.innerText = changeTurn() + " Wins!";
+            messageDiv.style.display = "block";
+
         }
     })
 }
@@ -38,14 +45,14 @@ const checkWin = ()=>{
 //game logic
 let boxes = document.getElementsByClassName('box');
 
-Array.from(boxes).forEach((element)=>{
+Array.from(boxes).forEach((element) => {
     let boxtext = element.querySelector('.boxtext')
-    element.addEventListener("click", ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener("click", () => {
+        if (boxtext.innerText === '') {
             boxtext.innerText = turn;
             turn = changeTurn()
             checkWin()
-            if(!gameover){
+            if (!gameover) {
                 document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
             }
         }
@@ -54,15 +61,16 @@ Array.from(boxes).forEach((element)=>{
 
 
 //add event listner to reset
-reset.addEventListener("click", (element)=>{
+reset.addEventListener("click", (element) => {
     let boxtexts = document.querySelectorAll('.boxtext')
     Array.from(boxtexts).forEach(element => {
         element.innerText = ''
     });
+    const messageDiv = document.getElementById("winnerMessage");
+    messageDiv.innerText = changeTurn() + " Wins!";
+    messageDiv.style.display = "none";
     turn = 'X';
     gameover = false
     document.querySelector(".line").style.width = "0";
     document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
-
 })
-
